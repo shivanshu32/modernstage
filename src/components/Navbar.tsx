bc1aac56d4c9899d8d42a242521b29aaca224b0d
AbscodInformatics <shivanshupathak@Shivanshus-MacBook-Air.local>
2025-06-05 21:52:38 +0530

    save

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -72,11 +72,13 @@ export default function Navbar() {
                 <div 
                   key={link.name} 
                   className="relative group"
-                  onMouseEnter={() => link.submenu && setActiveDropdown(link.name)}
-                  onMouseLeave={() => link.submenu && setActiveDropdown(null)}
                 >
                   {link.submenu ? (
-                    <>
+                    <div 
+                      className="relative"
+                      onMouseEnter={() => setActiveDropdown(link.name)}
+                      onMouseLeave={() => setActiveDropdown(null)}
+                    >
                       <button
                         onClick={() => setActiveDropdown(activeDropdown === link.name ? null : link.name)}
                         className="text-gray-300 hover:text-gray-100 px-3 py-2 rounded-md text-sm font-medium transition-colors flex items-center"
@@ -104,7 +106,7 @@ export default function Navbar() {
                           ))}
                         </div>
                       </div>
-                    </>
+                    </div>
                   ) : (
                     <Link
                       href={link.href}
